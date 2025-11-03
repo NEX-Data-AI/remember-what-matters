0ee5b1c76a098001a66d8a06210e6452a697c163
Donnie Arbeau <161102449+NEX-Data-AI@users.noreply.github.com>
2025-11-03 09:38:38 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,11 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
       <BrowserRouter>
+        <Sonner />
+        <Toaster />
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
